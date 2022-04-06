eedfe823c820b141001556d5f8054a8f3d582bd1
PakoSSS <school12a456@gmail.com>
2022-04-06 18:44:45 +0300

    Success!

--- AdrenalineDP/Controllers/ServicesController.cs
+++ AdrenalineDP/Controllers/ServicesController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace AdrenalineDP.Controllers
 {
-    [Authorize]
+    
     public class ServicesController : Controller
     {
         private readonly ApplicationDbContext _context;
